6be69e93ea86439f55d63f9916f6f9d89cea06f7
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-19 01:42:44 +0800

    update notification updates

--- src/Components/Header/Notification.jsx
+++ src/Components/Header/Notification.jsx
@@ -8,5 +8,12 @@ export default function Notification() {
   const notificationStyle = notificationDisplay
     ? { left: "calc(100vw - 350px)" }
     : null;
-  return <div className="notification" style={notificationStyle}></div>;
+  return (
+    <div className="notification" style={notificationStyle}>
+      <h1>Updates</h1>
+      <div className="updates">
+        <p>Nothing is here yet</p>
+      </div>
+    </div>
+  );
 }
