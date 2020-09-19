61a711ab6b010a92a079c67a56af04dbc3f73982
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-20 01:38:07 +0800

    added firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { HomeProvider } from "./ContextAPI/homeContext";
 import Home from "./Pages/Home/Home";
+import Auth from "./Pages/Home/Auth/Auth";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/:tag" component={Home} />
+            <Router exact path="/auth" components={Auth} />
           </Switch>
         </div>
       </Router>
