ba64bba634e4d4232ac2fc29adff2d172e2c7083
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-23 18:25:50 +0800

    update auth form

---
+++ src/Pages/Auth/SignInForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./SignInForm.scss"
+
+export default function SignInForm() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/Pages/Auth/SignUpForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./SignUpForm.scss"
+
+export default function SignUpForm() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
