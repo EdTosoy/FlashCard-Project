29f59f9096b0e06b9248c7af37c88b32bf70c544
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-23 18:20:55 +0800

    added components to authpage

---
+++ src/Pages/Auth/AuthHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// import "./"
+
+export default function AuthHeader() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/Pages/Auth/AuthNavigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+import "./AuthNavigation.scss"
+
+export default function AuthNavigation() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
