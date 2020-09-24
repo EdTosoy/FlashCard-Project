63e426d9594124b9803335bc3bee745f75b64db4
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-24 22:56:28 +0800

    update auth header

--- src/Pages/Auth/AuthHeader.jsx
+++ src/Pages/Auth/AuthHeader.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logo from "../../Components/Header/Logo"
 import AuthNavigation from './AuthNavigation'
 
 export default function AuthHeader() {
     return (
         <div className="auth-header">
+            <Logo/>
             <AuthNavigation/>
         </div>
     )
 }
+34
