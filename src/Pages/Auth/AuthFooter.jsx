e36a0ccf78570f15990a807e05350c9d80e8199c
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-25 07:15:42 +0800

    added auth Footer

--- src/Pages/Auth/AuthFooter.jsx
+++ src/Pages/Auth/AuthFooter.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import "./AuthFooter.scss"
+import "./AuthFooter.scss";
 
 export default function AuthFooter() {
-    return (
-        <div>
-            
-            
-        </div>
-    )
+  return (
+    <div className="auth-footer">
+      <li>Terms of Service</li>
+      <li>Privacy Policy</li>
+      <li>Help</li>
+      <li>iPhone App</li>
+      <li>Andriod App</li>
+      <li>Users</li>
+      <li>Collections</li>
+      <li>Explore</li>
+    </div>
+  );
 }
