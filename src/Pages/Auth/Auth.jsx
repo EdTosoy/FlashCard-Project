c9abaa5d91c77d3718028b14aa0aaf2241ec878f
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-27 23:30:33 +0800

    update auth

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -3,11 +3,17 @@ import React from "react";
 import "./Auth.scss";
 import AuthFooter from "./AuthFooter";
 import AuthHeader from "./AuthHeader";
+import SignInForm from "./SignInForm";
 
 export default function Auth() {
   return (
     <div className="auth">
       <AuthHeader />
+      <main>
+        <h1>Memories</h1>
+        <SignInForm/>
+      </main>
+
       <AuthFooter />
     </div>
   );
