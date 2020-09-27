4b7a4998c21e7a8deea1270046c92e489dea51de
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-27 23:35:04 +0800

    update auth

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Auth.scss";
 import AuthFooter from "./AuthFooter";
 import AuthHeader from "./AuthHeader";
 import SignInForm from "./SignInForm";
+import { useState } from "react";
+import SignUpForm from "./SignUpForm";
 
 export default function Auth() {
+  const [authStatus, setAuthStatus] = useState(false);
   return (
     <div className="auth">
       <AuthHeader />
       <main>
         <h1>Memories</h1>
-        <SignInForm/>
+        
+        {authStatus ? <SignInForm setAuthStatus={setAuthStatus}/> : <SignUpForm setAuthStatus={setAuthStatus} />}
       </main>
 
       <AuthFooter />
