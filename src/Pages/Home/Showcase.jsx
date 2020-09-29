c060795efb1a8a6412293979b1b1571c70cc362a
Ed Tosoy <edberto.tosoy@gmail.com>
2020-09-30 06:10:30 +0800

    update showcase

--- src/Pages/Home/Showcase.jsx
+++ src/Pages/Home/Showcase.jsx
@@ -1,14 +1,32 @@
 import React, { useContext } from "react";
 
+import Masonry from "react-masonry-css";
+
 import { HomeContext } from "../../ContextAPI/homeContext";
 
 import "./Showcase.scss";
 
 export default function Showcase() {
   const { darkTheme } = useContext(HomeContext);
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
   return (
     <div className={`showcase ${darkTheme && "dark-showcase"}`}>
-      <div className="card">card</div>
+      {/* <div className="card">card</div> */}
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        <div>My Element</div>
+        <div>My Element</div>
+        <div>My Element</div>
+        <div>My Element</div>
+      </Masonry>
     </div>
   );
 }
